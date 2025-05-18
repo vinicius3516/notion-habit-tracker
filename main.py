6b9d937bd46078c03e5f8883c10e2b3b72303d32
Vinicius Costa <costavinicius692@gmail.com>
2025-05-18 18:12:41 -0300

    Alterando token do notion

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ load_dotenv()
 
 NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 DATABASE_ID = os.getenv("DATABASE_ID")
-
 HEADERS = {
     "Authorization": f"Bearer {NOTION_TOKEN}",
     "Notion-Version": "2022-06-28",
